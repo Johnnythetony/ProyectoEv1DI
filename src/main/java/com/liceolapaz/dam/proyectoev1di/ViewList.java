d7770397dfdbd47faebfa525f29b2621fd1b0a8e
Johnnythetony <jamp1998@gmail.com>
2025-11-18 18:24:23 +0100

    Crear enum para guardar vistas de la aplicacion

---
+++ src/main/java/com/liceolapaz/dam/proyectoev1di/ViewList.java
@@ -0,0 +1,19 @@
+package com.liceolapaz.dam.proyectoev1di;
+
+public enum ViewList 
+{
+    LOGIN("login-view.fxml"),
+    REGISTER("register-view.fxml");
+
+    private final String fxml;
+
+    ViewList(String fxml)
+    {
+        this.fxml = fxml;
+    }
+
+    public String getFXML()
+    {
+        return fxml;
+    }
+}
