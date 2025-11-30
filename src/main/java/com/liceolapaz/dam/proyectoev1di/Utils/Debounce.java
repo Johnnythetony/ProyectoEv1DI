aa65e6c7c349845b5d79e835ffb1c9e330aaa28a
Johnnythetony <jamp1998@gmail.com>
2025-11-30 19:50:27 +0100

    Refactorizar Debounce

--- src/main/java/com/liceolapaz/dam/proyectoev1di/Utils/Debounce.java
+++ src/main/java/com/liceolapaz/dam/proyectoev1di/Utils/Debounce.java
@@ -9,17 +9,29 @@ import javafx.util.Duration;
 public class Debounce
 {
     private Timeline timeline;
-    private final int DEBOUNCE_MILLIS = 400;
+    private final int DEBOUNCE_MILLIS = 800;
+    private String newValue = "";
 
-    public Debounce(EventHandler<ActionEvent> evento)
+    public Debounce()
     {
-        timeline = new Timeline(new KeyFrame(Duration.millis(DEBOUNCE_MILLIS), evento));
+        timeline = new Timeline(new KeyFrame(Duration.millis(DEBOUNCE_MILLIS), e->{}));
         timeline.setCycleCount(1);
     }
 
-    public void debounce()
+    public void debounce(String newValue)
     {
+        this.newValue = newValue;
         timeline.stop();
         timeline.playFromStart();
+    }
+
+    public String getNewValue()
+    {
+        return newValue;
+    }
+
+    public void newKeyframe(EventHandler<ActionEvent> evento)
+    {
+        timeline.getKeyFrames().set(0, new KeyFrame(Duration.millis(DEBOUNCE_MILLIS),evento));
     }
 }
