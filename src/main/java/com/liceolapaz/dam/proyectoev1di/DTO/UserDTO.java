280bca34588c16ec03b3a6c79c257a576053b0d5
Johnnythetony <jamp1998@gmail.com>
2025-12-02 20:26:41 +0100

    Desechar información crítica del DTO principal de usuario

--- src/main/java/com/liceolapaz/dam/proyectoev1di/DTO/UserDTO.java
+++ src/main/java/com/liceolapaz/dam/proyectoev1di/DTO/UserDTO.java
@@ -2,25 +2,15 @@ package com.liceolapaz.dam.proyectoev1di.DTO;
 
 public class UserDTO
 {
-    private Long id;
     private String username;
-    private String password;
     private String mail;
-    private boolean admin;
     private boolean stylesheet;
 
-    public UserDTO(Long id, String username, String password, String mail, boolean admin, boolean stylesheet)
+    public UserDTO(String username, String mail)
     {
-        this.id = id;
         this.username = username;
-        this.password = password;
         this.mail = mail;
-        this.admin = admin;
-        this.stylesheet = stylesheet;
-    }
-
-    public Long getId() {
-        return id;
+        this.stylesheet = false;
     }
 
     public String getUsername() {
@@ -31,24 +21,12 @@ public class UserDTO
         this.username = username;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getMail() {
         return mail;
     }
 
     public void setMail(String mail) {
         this.mail = mail;
-    }
-
-    public boolean isAdmin() {
-        return admin;
     }
 
     public boolean isStylesheet()
