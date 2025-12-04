e8711eeafa5ea33c3dae379265345b448aceb0f2
Johnnythetony <jamp1998@gmail.com>
2025-12-04 20:30:44 +0100

    Crear y enlazar controladores

---
+++ src/main/java/com/liceolapaz/dam/proyectoev1di/Controllers/AccountViewController.java
@@ -0,0 +1,11 @@
+package com.liceolapaz.dam.proyectoev1di.Controllers;
+
+import javafx.event.ActionEvent;
+
+public class AccountViewController {
+    public void submit(ActionEvent actionEvent) {
+    }
+
+    public void cleanForm(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/liceolapaz/dam/proyectoev1di/Controllers/UserLibraryController.java
@@ -0,0 +1,4 @@
+package com.liceolapaz.dam.proyectoev1di.Controllers;
+
+public class UserLibraryController {
+}
